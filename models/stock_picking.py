d72cc898b2f4284b47a38a4a6178c76b22c84436
Nasim Rishal V P <nasimrishal27@gmail.com>
2025-03-28 10:33:00 +0530

    Dymanic tolerance

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -8,19 +8,22 @@ class StockPicking(models.Model):
 
     hide_validate_button = fields.Boolean(compute="_compute_hide_validate_button", store=True)
 
-    @api.depends('move_ids.quantity', "move_ids_without_package")
+    @api.depends("move_ids_without_package")
     def _compute_hide_validate_button(self):
+        """making the button hidden"""
         for picking in self:
-            if picking.move_ids.filtered(lambda l: l.quantity < 5 or l.quantity > 15):
+            tolerance = picking.sale_id.tolerance
+            if picking.move_ids.filtered(lambda l: l.quantity < l.product_uom_qty - tolerance
+                                                   or l.quantity > l.product_uom_qty + tolerance):
                 picking.hide_validate_button = True
             else:
                 picking.hide_validate_button = False
 
     def action_validate(self):
-        """"""
+        """button to open tolerance wizard"""
         view = self.env.ref('sale_tolerance.view_sale_order_confirmation')
         return {
-            'name': _('Sale Order Confirmation'),
+            'name': _('The quantity is outside tolerance range'),
             'type': 'ir.actions.act_window',
             'view_mode': 'form',
             'res_model': 'sale.order.confirmation',
